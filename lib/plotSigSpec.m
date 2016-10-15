e773c9fd3ce9567051e4cc5d527f28348b62522a
rezafara <faramarzi.reza14@gmail.com>
2016-10-15 23:05:51 +0330

    some Fixing!
    Directories were created.

--- lib/plotSigSpec.m
+++ lib/plotSigSpec.m
@@ -1,4 +1,4 @@
-function [ SIG ] = plotSigSpec( sig,Fs )
+function [ SIG ] = plotSigSpec( sig,Fs,lineSpec )
 %plotSigSpec Plots signal single sided spectra using matlab fft function 
 %   Sig : Input signal in time domain
 %   Fs  : Sampling frequency
@@ -9,7 +9,7 @@ SIG = fft(sig,NFFT)/numel(sig);
 f = Fs/2*linspace(0,1,NFFT/2+1);
 
 %Plotting time:
-plot(f,2*abs(SIG(1:NFFT/2+1)));
+plot(f,2*abs(SIG(1:NFFT/2+1)),lineSpec);
 xlabel('Frequency[Hz]');
 ylabel('|X(f)|');
 
